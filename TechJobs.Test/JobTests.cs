d9a2e22563667867f315bd7639848d87543dc855
Colleen Blake <colleenemilyblake@gmail.com>
2023-09-20 18:43:52 -0500

    about to run test 4

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -22,15 +22,27 @@ namespace TechJobs.Tests
         [TestMethod]
         public void TestSettingJobID()
         {
-            Assert.AreEqual(1, job_1.Id, 0.001);
-            Assert.AreEqual(2, job_2.Id, 0.001);
+            Assert.IsTrue(job_1.Id < job_2.Id);
+            Assert.IsTrue(job_2.Id - job_1.Id == 1);
+            //Assert.AreEqual(1, job_1.Id);
+            //Assert.AreEqual(2, job_2.Id);
             Assert.IsFalse(job_1 == job_2);
         }
 
         [TestMethod]
         public void TestJobConstructorSetsAllFields()
         {
+            Assert.AreEqual("Product tester", job_4.Name.ToString());
+            Assert.AreEqual("ACME", job_4.EmployerName.ToString());
+            Assert.AreEqual("Desert", job_4.EmployerLocation.ToString());
+            Assert.AreEqual("Quality control", job_4.JobType.ToString());
+            Assert.AreEqual("Persistence", job_4.JobCoreCompetency.ToString());
+        }
 
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(job_1.Equals(job_2));
         }
     }
 }
--- TechJobsOOAutoGraded6/Program.cs
+++ TechJobsOOAutoGraded6/Program.cs
@@ -4,4 +4,5 @@ using TechJobsOOAutoGraded6;
 RunTechJobs runNewTechJob = new RunTechJobs();
 runNewTechJob.RunProgram();
 
-
+Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+Console.WriteLine(job.Id.ToString());
