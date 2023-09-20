e000840d54bafcbce1dc1e37f2d68fa33cc1bd6a
Colleen Blake <colleenemilyblake@gmail.com>
2023-09-20 16:23:02 -0500

    Working thru part 4

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -4,13 +4,33 @@ namespace TechJobs.Tests
 	[TestClass]
 	public class JobTests
 	{
-        //Testing Objects
-        //initalize your testing objects here
+        Job job_1;
+        Job job_2;
+        Job job_3;
+        Job job_4;
+        [TestInitialize]
+        public void CreateJobObject()
+        {
+            job_1 = new Job();
+            job_2 = new Job();
+            job_3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            job_4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        }
+            //Testing Objects
+        //initialize your testing objects here
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobID()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Assert.AreEqual(1, job_1.Id, 0.001);
+            Assert.AreEqual(2, job_2.Id, 0.001);
+            Assert.IsFalse(job_1 == job_2);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+
         }
     }
 }
